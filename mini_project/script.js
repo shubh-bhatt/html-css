c9290a76c5d0936e2be761891d5311f73aae6b83
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-07-02 16:20:44 +0530

    project add

---
+++ mini_project/script.js
@@ -0,0 +1,7 @@
+const main1 = document.querySelector(".main1");
+const main2 = document.querySelector(".main2");
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  
+});
